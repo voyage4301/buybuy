4afa889817efa25515abe99e3b02392d15966458
voyage4301 <815717711@qq.com>
2018-07-06 23:27:26 +0800

    全部功能完成

--- js/inlanddiscount.js
+++ js/inlanddiscount.js
@@ -1,18 +1,49 @@
 $(function () {
 
+    var datas = []
+    var pageSize = 8,
+        start = 0
 
-    function render() {
+
+    function getAll() {
         $.ajax({
             type: 'get',
             url: 'http://127.0.0.1:9090/api/getinlanddiscount',
             success: function (info) {
-                console.log(info);
-                
-                $('.mm_content ul').html(template('inlanddiscountList', {
-                    rows: info.result
-                }))
+                datas = info.result
+                var data = datas.splice(start, pageSize)
+                console.log(data);
+
+                $('.mm_content').append('<img class="load" src="./images/loader.gif" alt="">')
+
+                setTimeout(() => {
+                    $('.mm_content .load').remove()
+                    if (data.length === 0) {
+                        $('.mm_content').append('<p class="tips">没有更多数据了</p>')
+                        return false
+                    }
+                    $('.mm_content ul').append(template('inlanddiscountList', {
+                        rows: data
+                    }))
+                    start += pageSize
+                    flag = true
+                }, 1500);
             }
         })
     }
-    render()
+    getAll()
+
+    var flag = true
+    $(window).on('scroll', function () {
+        var top = $('.mm_content ul li:last-child').offset().top //最后一个li离文档的距离
+        var liHeight = $('.mm_content ul li:last-child').height() //li的高
+        var wHeight = $(window).height() //可视区的高
+        var val = wHeight - liHeight
+        if ((val - top) > 200 && flag) {
+            flag = false
+            getAll()
+        }
+    })
+
+
 })
